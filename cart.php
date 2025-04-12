b195abdb6c8a37e7989818d3aa8e70c6316b5636
Luan Lee <huuluan.leminh28805@gmail.com>
2025-04-12 21:55:48 +0700

    Sửa nhẹ cart

--- cart.php
+++ cart.php
@@ -5,12 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/cart.css">
-    <!-- Chỉ load Bootstrap một lần để tránh trùng lặp -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <title>Shopping Cart</title>
+    <link rel="stylesheet" href="fonts_icon/fontawesome-free-6.7.1-web/fontawesome-free-6.7.1-web/css/all.css">
+    <title>Giỏ Hàng</title>
 </head>
 <body>
 <div class="cart-container">
@@ -19,8 +15,8 @@
             <div class="col-md-8 cart">
                 <div class="title">
                     <div class="row">
-                        <div class="col"><h4><b>Giỏ hàng</b></h4></div>
-                        <div class="col align-self-center text-right text-muted">3 items</div>
+                        <div class="col"><h4><b>Giỏ Hàng</b></h4></div>
+                        <div class="col align-self-center text-right text-muted">3 sản phẩm</div>
                     </div>
                 </div>    
                 <div class="row border-top border-bottom">
@@ -51,10 +47,10 @@
                 </div>
                 <div class="row border-top border-bottom">
                     <div class="row main align-items-center">
-                        <div class="col-2"><img class="img-fluid" src="./img//plates/w-hp-mug-10-2.jpg"></div>
+                        <div class="col-2"><img class="img-fluid" src="./img/plates/w-hp-mug-10-2.jpg"></div>
                         <div class="col">
                             <div class="row text-muted">Dĩa</div>
-                            <div class="row">DĨa đéo gì cũng không biết tên</div>
+                            <div class="row">Dĩa đéo gì cũng không biết tên</div>
                         </div>
                         <div class="col">
                             <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a>
@@ -62,33 +58,33 @@
                         <div class="col">€ 44.00 <span class="close">✕</span></div>
                     </div>
                 </div>
-                <div class="back-to-shop"><a href="#">←<span class="text-muted">Tiếp tục mua sắm</span></a></div>
+                <div class="back-to-shop"><a href="shop.php">←<span class="text-muted">Tiếp tục mua sắm</span></a></div>
             </div>
             <div class="col-md-4 summary">
-                <div><h5><b>Summary</b></h5></div>
+                <div><h5><b>Tóm Tắt Đơn Hàng</b></h5></div>
                 <hr>
                 <div class="row">
-                    <div class="col" style="padding-left:0;">ITEM : 3</div>
+                    <div class="col" style="padding-left:0;">SỐ LƯỢNG: 3</div>
                     <div class="col text-right">€ 132.00</div>
                 </div>
-                <form>
-                    <p>SHIPPING</p>
-                    <select>
-                         <option class="text-muted">Giao hàng tiêu chuẩn - €5.00</option>
-                         <option class="text-muted">Giao hàng nhanh - €8.00 </option>
-                         <option class="text-muted">Hỏa tốc - €14.99 </option>
-                         <option class="text-muted">Giao hàng miễn phí - €0.00 (1 tháng sau có hàng)</option>
+                <form  method="POST">
+                    <p>PHÍ VẬN CHUYỂN</p>
+                    <select name="shipping">
+                        <option value="5.00">Giao hàng tiêu chuẩn - €5.00</option>
+                        <option value="8.00">Giao hàng nhanh - €8.00</option>
+                        <option value="14.99">Hỏa tốc - €14.99</option>
+                        <option value="0.00">Giao hàng miễn phí - €0.00 (1 tháng sau có hàng)</option>
                     </select>
                     <p>CODE</p>
-                    <input id="code" placeholder="Enter your code">
+                    <input id="code" name="code" placeholder="Nhập mã giảm giá">
+                    <div class="row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
+                        <div class="col">TỔNG CỘNG</div>
+                        <div class="col text-right">€ 137.00</div>
+                    </div>
+                    <button type="submit" class="btn">CHECKOUT</button>
                 </form>
-                <div class="row" style="border-top: 1px solid rgba(0,0,0,.1); padding: 2vh 0;">
-                    <div class="col">TOTAL PRICE</div>
-                    <div class="col text-right">€ 137.00</div>
-                </div>
-                <button class="btn">CHECKOUT</button>
             </div>
-        </div> 
+        </div>
     </div>
 </div>
 <?php include("footer.php"); ?>
